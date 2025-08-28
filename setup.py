e349d385a976e5afa27d6d4b4cbda5395590d744
avinash <avinash.kamath@scalekit.com>
2025-08-28 18:12:50 +0530

    update release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="a-beta-scalekit",
-    version="4.0.1",
+    version="4.0.2",
     packages=find_packages(),
     install_requires=[
         "grpcio>=1.64.1",
